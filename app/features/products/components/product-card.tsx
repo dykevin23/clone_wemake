32d8a1d75e687d117c7e59a9d103a95178228da5
kevin <dyk0203@naver.com>
2025-04-01 14:47:15 +0900

    Create PostCard

--- app/features/products/components/product-card.tsx
+++ app/features/products/components/product-card.tsx
@@ -28,11 +28,9 @@ export function ProductCard({
 }: ProductCardProps) {
   return (
     <Link to={`/products/${id}`}>
-      <Card className="w-full flex items-center justify-between bg-transparent hover:bg-card/50">
+      <Card className="w-full flex flex-row items-center justify-between bg-transparent hover:bg-card/50">
         <CardHeader>
-          <CardTitle className="text-2xl font-semibold leading-none tracking-tight">
-            {name}
-          </CardTitle>
+          <CardTitle>{name}</CardTitle>
           <CardDescription className="text-muted-foreground">
             {description}
           </CardDescription>
