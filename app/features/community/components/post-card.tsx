c88bb4e85b3ef62ef663666444b86d88db545e01
kevin <dyk0203@naver.com>
2025-04-02 09:13:16 +0900

    Create IdeasCard

--- app/features/community/components/post-card.tsx
+++ app/features/community/components/post-card.tsx
@@ -48,9 +48,7 @@ export function PostCard({
           </div>
         </CardHeader>
         <CardFooter className="flex justify-end">
-          <Button variant="link" asChild>
-            <Link to={`/community/${id}`}>Reply &rarr;</Link>
-          </Button>
+          <Button variant="link">Reply &rarr;</Button>
         </CardFooter>
       </Card>
     </Link>
