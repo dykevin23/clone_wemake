9029f82f85cfc38eb46f558b94e2f07391c08de3
kevin <dyk0203@naver.com>
2025-04-24 09:11:04 +0900

    review pages

--- app/features/products/pages/product-reviews-page.tsx
+++ app/features/products/pages/product-reviews-page.tsx
@@ -1,3 +1,35 @@
+import { Button } from "~/common/components/ui/button";
+import ReviewCard from "../components/review-card";
+
+export function meta() {
+  return [
+    { title: "Product Reviews | wemake" },
+    { name: "description", content: "Read and write product reviews" },
+  ];
+}
+
 export default function ProductReviewsPage() {
-  return <div>reviews</div>;
+  return (
+    <div className="space-y-10 max-w-xl">
+      <div className="flex justify-between items-center">
+        <h2 className="text-2xl font-bold">10 Reviews</h2>
+        <Button variant="secondary">Write a review</Button>
+      </div>
+      <div className="space-y-20">
+        {Array.from({ length: 10 }).map((_, index) => (
+          <ReviewCard
+            username="John Doe"
+            handle="@username"
+            avatarUrl="https://github.com/facebook.png"
+            rating={5}
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
+        Quos.Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
+        Quos.Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
+        Quos."
+            postedAt="10 days ago"
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
