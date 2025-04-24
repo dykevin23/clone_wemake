a4cf6ce8b0d4c14fd3211dc839ed2016612f4e4a
kevin <dyk0203@naver.com>
2025-04-24 09:58:30 +0900

    product review

--- app/features/products/pages/product-reviews-page.tsx
+++ app/features/products/pages/product-reviews-page.tsx
@@ -1,5 +1,7 @@
 import { Button } from "~/common/components/ui/button";
 import ReviewCard from "../components/review-card";
+import { Dialog, DialogTrigger } from "~/common/components/ui/dialog";
+import CreateReviewDialog from "../components/create-review-dialog";
 
 export function meta() {
   return [
@@ -10,26 +12,31 @@ export function meta() {
 
 export default function ProductReviewsPage() {
   return (
-    <div className="space-y-10 max-w-xl">
-      <div className="flex justify-between items-center">
-        <h2 className="text-2xl font-bold">10 Reviews</h2>
-        <Button variant="secondary">Write a review</Button>
-      </div>
-      <div className="space-y-20">
-        {Array.from({ length: 10 }).map((_, index) => (
-          <ReviewCard
-            username="John Doe"
-            handle="@username"
-            avatarUrl="https://github.com/facebook.png"
-            rating={5}
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
+    <Dialog>
+      <div className="space-y-10 max-w-xl">
+        <div className="flex justify-between items-center">
+          <h2 className="text-2xl font-bold">10 Reviews</h2>
+          <DialogTrigger>
+            <Button variant="secondary">Write a review</Button>
+          </DialogTrigger>
+        </div>
+        <div className="space-y-20">
+          {Array.from({ length: 10 }).map((_, index) => (
+            <ReviewCard
+              username="John Doe"
+              handle="@username"
+              avatarUrl="https://github.com/facebook.png"
+              rating={5}
+              content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
         Quos.Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
         Quos.Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
         Quos."
-            postedAt="10 days ago"
-          />
-        ))}
+              postedAt="10 days ago"
+            />
+          ))}
+        </div>
       </div>
-    </div>
+      <CreateReviewDialog />
+    </Dialog>
   );
 }
