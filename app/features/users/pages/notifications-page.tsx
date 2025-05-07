509c2d4a3bd390be3b4c9d08af864d40bbbbf210
kevin <dyk0203@naver.com>
2025-05-07 09:17:21 +0900

    notification page

--- app/features/users/pages/notifications-page.tsx
+++ app/features/users/pages/notifications-page.tsx
@@ -1,5 +1,24 @@
-export const meta: Route.MetaFunction = () => [{ title: " | wemake" }];
+import type { Route } from "./+types/notifications-page";
+
+import { NotificationCard } from "../components/notification-card";
+
+export const meta: Route.MetaFunction = () => [
+  { title: "Notifications | wemake" },
+];
 
 export default function NotificationsPage() {
-  return <></>;
+  return (
+    <div className="space-y-20">
+      <h1 className="text-4xl font-bold">Notifications</h1>
+      <div className="flex flex-col items-start gap-5">
+        <NotificationCard
+          avatarUrl="https://github.com/stevejobs.png"
+          avatarFallback="S"
+          userName="Steve Jobs"
+          message=" followed you."
+          timestamp="2 days ago"
+        />
+      </div>
+    </div>
+  );
 }
