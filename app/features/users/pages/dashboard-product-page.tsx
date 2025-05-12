5657def19021e567b2b4fa3f924bcb3d4f7c6b7c
kevin <dyk0203@naver.com>
2025-05-13 08:46:48 +0900

    product dashboard

--- app/features/users/pages/dashboard-product-page.tsx
+++ app/features/users/pages/dashboard-product-page.tsx
@@ -1,5 +1,96 @@
-export const meta: Route.MetaFunction = () => [{ title: " | wemake" }];
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from "~/common/components/ui/card";
+import type { Route } from "./+types/dashboard-product-page";
+import {
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "~/common/components/ui/chart";
+import {
+  Area,
+  AreaChart,
+  CartesianGrid,
+  Line,
+  LineChart,
+  XAxis,
+} from "recharts";
+
+export const meta: Route.MetaFunction = () => [
+  { title: "Product Dashboard | wemake" },
+];
+
+const chartData = [
+  { month: "Januaray", views: 106, visitors: 100 },
+  { month: "Febuaray", views: 305, visitors: 22 },
+  { month: "March", views: 237, visitors: 39 },
+  { month: "April", views: 73, visitors: 284 },
+  { month: "May", views: 209, visitors: 56 },
+  { month: "June", views: 214, visitors: 134 },
+];
+
+const chartConfig = {
+  views: {
+    label: "Page Views",
+    color: "var(--chart-1)",
+  },
+  visitors: {
+    label: "Visitors",
+    color: "var(--chart-2)",
+  },
+} satisfies ChartConfig;
 
 export default function DashboardProductPage() {
-  return <></>;
+  return (
+    <div className="space-y-5">
+      <h1 className="text-2xl font-semibold mb-6">Analytics</h1>
+      <Card className="w-1/2">
+        <CardHeader>
+          <CardTitle>Performance</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <ChartContainer config={chartConfig}>
+            <AreaChart
+              accessibilityLayer
+              data={chartData}
+              margin={{ left: 12, right: 12 }}
+            >
+              <CartesianGrid vertical={false} />
+              <XAxis
+                dataKey="month"
+                tickLine={false}
+                axisLine={false}
+                tickMargin={8}
+                tickFormatter={(v) => v.slice(0, 3)}
+              />
+              <Area
+                dataKey="views"
+                type="natural"
+                stroke="var(--color-views)"
+                fill="var(--color-views)"
+                strokeWidth={2}
+                dot={false}
+              />
+              <Area
+                dataKey="visitors"
+                type="natural"
+                stroke="var(--color-visitors)"
+                fill="var(--color-visitors)"
+                strokeWidth={2}
+                dot={false}
+              />
+              <ChartTooltip
+                cursor={false}
+                wrapperStyle={{ minWidth: "200px" }}
+                content={<ChartTooltipContent indicator="dot" />}
+              />
+            </AreaChart>
+          </ChartContainer>
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
