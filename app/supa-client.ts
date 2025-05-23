81f1e275f50a62e2712066e2ca343d16e62b9a7a
kevin <dyk0203@naver.com>
2025-05-23 09:02:25 +0900

    type fest

--- app/supa-client.ts
+++ app/supa-client.ts
@@ -1,5 +1,29 @@
 import { createClient } from "@supabase/supabase-js";
-import { type Database } from "database.types";
+import {
+  type MergeDeep,
+  type SetFieldType,
+  type SetNonNullable,
+} from "type-fest";
+import { type Database as SupabaseDatabase } from "database.types";
+
+type Database = MergeDeep<
+  SupabaseDatabase,
+  {
+    public: {
+      Views: {
+        community_post_list_view: {
+          Row: SetFieldType<
+            SetNonNullable<
+              SupabaseDatabase["public"]["Views"]["community_post_list_view"]["Row"]
+            >,
+            "author_avatar",
+            string | null
+          >;
+        };
+      };
+    };
+  }
+>;
 
 export const client = createClient<Database>(
   process.env.SUPABASE_URL!,
