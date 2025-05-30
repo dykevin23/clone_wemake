53e10834fe24ebdaa9cee180007ac49ee4748743
kevin <dyk0203@naver.com>
2025-05-30 10:09:16 +0900

    ideas page

--- app/features/ideas/components/idea-card.tsx
+++ app/features/ideas/components/idea-card.tsx
@@ -1,4 +1,5 @@
 import { DotIcon, EyeIcon, HeartIcon, LockIcon } from "lucide-react";
+import { DateTime } from "luxon";
 import { Link } from "react-router";
 import { Button } from "~/common/components/ui/button";
 import {
@@ -11,7 +12,7 @@ import {
 import { cn } from "~/lib/utils";
 
 interface IdeaCardProps {
-  id: string;
+  id: number;
   title: string;
   viewsCount: number;
   postedAt: string;
@@ -50,7 +51,7 @@ export function IdeaCard({
           <span>{viewsCount}</span>
         </div>
         <DotIcon className="w-4 h-4" />
-        <span>{postedAt}</span>
+        <span>{DateTime.fromISO(postedAt).toRelative()}</span>
       </CardContent>
       <CardFooter className="flex justify-end gap-2">
         <Button variant="outline">
--- app/features/ideas/pages/idea-page.tsx
+++ app/features/ideas/pages/idea-page.tsx
@@ -2,35 +2,43 @@ import { Hero } from "~/common/components/hero";
 import type { Route } from "./+types/idea-page";
 import { DotIcon, EyeIcon, HeartIcon } from "lucide-react";
 import { Button } from "~/common/components/ui/button";
+import { getGptIdea } from "../queries";
+import { DateTime } from "luxon";
 
-export const meta: Route.MetaFunction = () => [
-  { title: "IdeasGPT | wemake" },
+export const meta: Route.MetaFunction = ({
+  data: {
+    idea: { gpt_idea_id, idea },
+  },
+}: Route.MetaArgs) => [
+  { title: `Idea #${gpt_idea_id}: ${idea} | wemake` },
   { name: "description", content: "Find ideas for your next project" },
 ];
 
-export default function IdeaPage() {
+export const loader = async ({ params }: Route.LoaderArgs) => {
+  const idea = await getGptIdea(params.ideaId);
+  return { idea };
+};
+
+export default function IdeaPage({ loaderData }: Route.ComponentProps) {
   return (
     <div className="space-y-20">
-      <Hero title="Idea #12121222" />
+      <Hero title={`Idea #${loaderData.idea.gpt_idea_id}`} />
       <div className="max-w-screen-sm mx-auto flex flex-col items-center gap-10">
-        <p className="italic">
-          "A startup that creates an AI-powered generated personal trainer,
-          delivering customized fitness recommendations and tracking of progress
-          using a mobile app to track workouts and progress as well as a website
-          to manage the business."
-        </p>
+        <p className="italic">"{loaderData.idea.idea}"</p>
 
         <div className="flex items-center text-sm">
           <div className="flex items-center gap-1">
             <EyeIcon className="w-4 h-4" />
-            <span>123</span>
+            <span>{loaderData.idea.views}</span>
           </div>
           <DotIcon className="w-4 h-4" />
-          <span>12 hours ago</span>
+          <span>
+            {DateTime.fromISO(loaderData.idea.created_at).toRelative()}
+          </span>
           <DotIcon className="w-4 h-4" />
           <Button variant="outline">
             <HeartIcon className="w-4 h-4" />
-            <span>12</span>
+            <span>{loaderData.idea.likes}</span>
           </Button>
         </div>
         <Button size="lg">Claim idea now &rarr;</Button>
