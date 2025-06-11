525e92fe849d2ee6f4065ea187e4a9ae265b3b60
kevin <dyk0203@naver.com>
2025-06-11 11:09:24 +0900

    user posts

--- app/features/users/pages/profile-post-page.tsx
+++ app/features/users/pages/profile-post-page.tsx
@@ -1,19 +1,27 @@
 import { PostCard } from "~/features/community/components/post-card";
 import type { Route } from "./+types/profile-post-page";
+import { getUserPosts } from "../queries";
 
 export const meta: Route.MetaFunction = () => [{ title: "Posts | wemake" }];
 
-export default function ProfilePostPage() {
+export const loader = async ({ params }: Route.LoaderArgs) => {
+  const posts = await getUserPosts(params.username);
+  return { posts };
+};
+
+export default function ProfilePostPage({ loaderData }: Route.ComponentProps) {
   return (
     <div className="flex flex-col gap-5">
-      {Array.from({ length: 5 }).map((_, index) => (
+      {loaderData.posts.map((post) => (
         <PostCard
-          id={`postId-${index}`}
-          title="What is the best productivity tool?"
-          author="Nico"
-          authorAvatarUrl="https://github.com/apple.png"
-          category="Productivity"
-          postedAt="12 hours ago"
+          key={post.post_id}
+          id={post.post_id}
+          title={post.title}
+          author={post.author}
+          authorAvatarUrl={post.author_avatar}
+          category={post.topic}
+          postedAt={post.created_at}
+          votesCount={post.upvotes}
           expanded
         />
       ))}
