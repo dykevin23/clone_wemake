ddf22c34341be901c4e249f3af3c2c0dfcf29ab8
kevin <dyk0203@naver.com>
2025-06-30 11:13:13 +0900

    file uploads

--- app/features/users/pages/settings-page.tsx
+++ app/features/users/pages/settings-page.tsx
@@ -9,7 +9,7 @@ import { Button } from "~/common/components/ui/button";
 import { makeSSRClient } from "~/supa-client";
 import { getLoggedInUserId, getUserById } from "../queries";
 import { z } from "zod";
-import { updateUser } from "../mutations";
+import { updateUser, updateUserAvatar } from "../mutations";
 import {
   Alert,
   AlertDescription,
@@ -36,26 +36,48 @@ export const action = async ({ request }: Route.ActionArgs) => {
   const { client } = makeSSRClient(request);
   const userId = await getLoggedInUserId(client);
   const formData = await request.formData();
-  const { success, error, data } = formSchema.safeParse(
-    Object.fromEntries(formData)
-  );
-  if (!success) {
-    return { formErrors: error.flatten().fieldErrors };
+  const avatar = formData.get("avatar");
+  if (avatar && avatar instanceof File) {
+    if (avatar.size <= 2097152 && avatar.type.startsWith("image/")) {
+      const { data, error } = await client.storage
+        .from("avatars")
+        .upload(userId, avatar, {
+          contentType: avatar.type,
+          upsert: true,
+        });
+      console.log(error);
+      if (error) {
+        return { formErrors: { avatar: ["Invalid file size or type"] } };
+      }
+      const {
+        data: { publicUrl },
+      } = await client.storage.from("avatars").getPublicUrl(data.path);
+      await updateUserAvatar(client, { id: userId, avatarUrl: publicUrl });
+    } else {
+      return { formErrors: { avatar: ["Invalid file size or type"] } };
+    }
+  } else {
+    const { success, error, data } = formSchema.safeParse(
+      Object.fromEntries(formData)
+    );
+    if (!success) {
+      return { formErrors: error.flatten().fieldErrors };
+    }
+    const { name, role, headline, bio } = data;
+    await updateUser(client, {
+      id: userId,
+      name,
+      role: role as
+        | "developer"
+        | "designer"
+        | "product-manager"
+        | "founder"
+        | "marketer",
+      headline,
+      bio,
+    });
+    return { ok: true };
   }
-  const { name, role, headline, bio } = data;
-  await updateUser(client, {
-    id: userId,
-    name,
-    role: role as
-      | "developer"
-      | "designer"
-      | "product-manager"
-      | "founder"
-      | "marketer",
-    headline,
-    bio,
-  });
-  return { ok: true };
 };
 
 export default function SettingsPage({
@@ -92,11 +114,11 @@ export default function SettingsPage({
               defaultValue={loaderData.user.name}
               placeholder="John Doe"
             />
-            {actionData?.formErrors?.name ? (
+            {actionData?.formErrors && "name" in actionData?.formErrors ? (
               <Alert>
                 <AlertTitle>Error</AlertTitle>
                 <AlertDescription>
-                  {actionData.formErrors.name.join(", ")}
+                  {actionData.formErrors?.name?.join(", ")}
                 </AlertDescription>
               </Alert>
             ) : null}
@@ -114,11 +136,11 @@ export default function SettingsPage({
                 { label: "Marketer", value: "marketer" },
               ]}
             />
-            {actionData?.formErrors?.role ? (
+            {actionData?.formErrors && "role" in actionData?.formErrors ? (
               <Alert>
                 <AlertTitle>Error</AlertTitle>
                 <AlertDescription>
-                  {actionData.formErrors.role.join(", ")}
+                  {actionData.formErrors?.role?.join(", ")}
                 </AlertDescription>
               </Alert>
             ) : null}
@@ -132,11 +154,11 @@ export default function SettingsPage({
               placeholder="John Doe"
               textArea
             />
-            {actionData?.formErrors?.headline ? (
+            {actionData?.formErrors && "headline" in actionData?.formErrors ? (
               <Alert>
                 <AlertTitle>Error</AlertTitle>
                 <AlertDescription>
-                  {actionData.formErrors.headline.join(", ")}
+                  {actionData.formErrors?.headline?.join(", ")}
                 </AlertDescription>
               </Alert>
             ) : null}
@@ -150,18 +172,22 @@ export default function SettingsPage({
               placeholder="John Doe"
               textArea
             />
-            {actionData?.formErrors?.bio ? (
+            {actionData?.formErrors && "bio" in actionData?.formErrors ? (
               <Alert>
                 <AlertTitle>Error</AlertTitle>
                 <AlertDescription>
-                  {actionData.formErrors.bio.join(", ")}
+                  {actionData.formErrors?.bio?.join(", ")}
                 </AlertDescription>
               </Alert>
             ) : null}
             <Button className="w-full">Update profile</Button>
           </Form>
         </div>
-        <aside className="col-span-2 p-6 rounded-lg border shadow-md">
+        <Form
+          className="col-span-2 p-6 rounded-lg border shadow-md"
+          method="post"
+          encType="multipart/form-data"
+        >
           <Label className="flex flex-col gap-1">
             Avatar
             <small className="text-muted-foreground">
@@ -180,8 +206,16 @@ export default function SettingsPage({
               className="w-1/2"
               onChange={onChange}
               required
-              name="icon"
+              name="avatar"
             />
+            {actionData?.formErrors && "avatar" in actionData?.formErrors ? (
+              <Alert>
+                <AlertTitle>Error</AlertTitle>
+                <AlertDescription>
+                  {actionData.formErrors.avatar.join(", ")}
+                </AlertDescription>
+              </Alert>
+            ) : null}
             <div className="flex flex-col text-xs">
               <span className="text-muted-foreground">
                 Recommended size: 128x128px
@@ -193,7 +227,7 @@ export default function SettingsPage({
             </div>
           </div>
           <Button className="w-full">Update avatar</Button>
-        </aside>
+        </Form>
       </div>
     </div>
   );
